2233d4b3ea845d37c358d7d137e0a0da79e9a85f
Mounika <mounikaendukuri03@gmail.com>
2024-04-27 19:37:01 +0200

    first program

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #writing my first script
-person1=ramesh
-person2=suresh
+person1=mounika
+person2=tarun
 echo "$person1 :: Hello $person2"
 echo "$person2 :: good to know $person1"
 echo "$person1 ::ohh nice of you"

5f0c5e7a6a3e858d7b24f9f506678f5c85df0d3f
Mounika <mounikaendukuri03@gmail.com>
2024-04-27 19:19:45 +0200

    first program

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #writing my first script
-
-echo "ramesh :: Hello suresh"
-echo "suresh :: good to know ramesh"
-echo "ramesh ::ohh nice of you"
+person1=ramesh
+person2=suresh
+echo "person1 :: Hello person2"
+echo "person2 :: good to know person1"
+echo "person1 ::ohh nice of you"

0bf544a8b94752e483aced69fa032f1ca425feeb
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 16:27:36 +0200

    program

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -7,23 +7,6 @@ then
  echo "number is less than 10"
 else
 echo "number is greater than 10"
-
 fi
 
-number2=$2
-
-if ($number2 -ge 20)
-then 
-echo "number is greater than or equal to 20 "
-else 
-echo "number is less than 20"
-fi
-
-number3=$3
-if ($number3 -gt 50)
-then 
-echo "number is greater than 50"
-else
-echo "number is less than 50"
-fi
 

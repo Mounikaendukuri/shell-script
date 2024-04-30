ab4385c13824131be2f908f8c995b0abe1b392d9
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 14:50:31 +0200

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-#writing my first script
-person1=mounika
-person2=tarun
-echo "$person1 :: Hello $person2"
-echo "$person2 :: good to know $person1"
-echo "$person1 ::ohh nice of you"
+#my first variables program and i'm sending arguments from outside to the persons as variable
+
+
+person1=Tarun
+person2=Mounika
+
+echo "$person1 : Hi $person2"
+echo "$person2 :hello $person1 and how are you??"
+echo "$person1 :i'm doing great thank you!!"
+echo "$person2 :how was your day in office ?"
+echo "$person1 :Not bad as usual!!"

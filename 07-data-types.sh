5cd8e954eda662c048c262c473ac598cb955b2ed
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 15:32:18 +0200

    program

---
+++ 07-data-types.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NO1=$1
+NO2=$2
+
+SUM=$(($NO1+$NO2))
+
+echo "Total of $NO1 and $NO2 is: $SUM"
+
+number1=10
+number2=20
+
+sum=$(($number1+number2))
+
+echo "total is $number1 and $number2 is :$sum"

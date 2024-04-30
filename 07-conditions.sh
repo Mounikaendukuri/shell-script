89a96532f5dd0478f2c80f91e585cdb6acc4dc6a
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 16:18:24 +0200

    program

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,10 +1,29 @@
-#!/bin/bash
+#!bin/bash
 
-NUMBER=$1
+number1=$1
 
-if [ $NUMBER -gt 10 ]
-then 
-echo "given number is greater than 10 :$NUMBER"
+if ($number1 -lt 10)
+then
+ echo "number is less than 10"
 else
-echo "given number is less than 10:$NUMBER"
+echo "number is greater than 10"
+
 fi
+
+number2=$2
+
+if ($number2 -ge 20)
+then 
+echo "number is greater than or equal to 20 "
+else 
+echo "number is less than 20"
+fi
+
+number3=$3
+if ($number3 -gt 50)
+then 
+echo "number is greater than 50"
+else
+echo "number is less than 50"
+fi
+

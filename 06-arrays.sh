0d3df6691a351a49618c511d5c905ccabaa76d8f
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 15:24:54 +0200

    program

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -4,11 +4,11 @@
 
 family=("mounika" "Tarun" "Radha" "geetha" "naga malla reddy")
 
-echo "Hello to $family"
-echo "good day to $@"
+echo "Hello to $0"
+echo "good day to ${family[@]}"
 
 echo "i love my mother ${family[2]} than anyone else"
 
 echo "my husband is ${family[1]}"
 
-echo "my mother-in law and father-in-law is ${family[3 4]} "
+echo "my mother-in law and father-in-law is ${family[3]} ${family[4]}"

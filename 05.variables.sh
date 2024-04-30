4b4ddedf919f44efb8a004f2c5bf50ab6b4c1552
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 15:08:26 +0200

    program

--- 05.variables.sh
+++ 05.variables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "enter the username : "
+#passing arguments in terminal itself and not in file for security reasons
+
+echo "enter your username :"
 read USERNAME
 
 echo "enter your password :"
-
 read PASSWORD
 
-echo "username is : $USERNAME & password is $PASSWORD"
+echo "the username is $USERNAME and the password is $PASSWORD"

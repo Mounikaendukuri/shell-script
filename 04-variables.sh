2fbbc44b32c0d60e256cd985e42c0f1c80a80153
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 14:57:26 +0200

    program

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #happy to learn
-person1=$0
-person2=$3
+person1=$1
+person2=$2
 echo "$person1 :: Hello $person2"
 echo "$person2 :: good to know $person1"
 echo "$person1 ::ohh nice of you"

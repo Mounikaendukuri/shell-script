138e7246002df6b0f0cc35a3af909ee138c56905
Mounika <mounikaendukuri03@gmail.com>
2024-04-30 19:59:02 +0200

    program

--- installation.sh
+++ installation.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
+USERID=$(id-u)
+
+if ( $USERID -ne 0 )
+then 
+  echo "please run the script with root access"
+  exit 2
+else 
+    echo "you are root user"
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
 then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    echo "Installation of mysql...FAILURE"
+    exit 5
 else
-    echo "You are super user."
+    echo "Installation of mysql...SUCCESS"
 fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 127
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
+echo "is script proceeding?"

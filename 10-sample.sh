ad658247d3ab658e481bad20beec91b4a25b715f
Mounika <mounikaendukuri03@gmail.com>
2024-05-01 18:15:16 +0200

    some-msg

---
+++ 10-sample.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date+%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[32m"
+G="\e[33m"
+Y="\e[34m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $? -ne 0]
+    then
+        echo" -e $2 is $R failure $N"
+    else
+        echo"-e $2 is $G sucess $N"
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+for i in $@
+do
+    echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done

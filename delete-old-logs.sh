4aca3e50749bd035998aab8bc54c25886335bb5f
Mounika <mounikaendukuri03@gmail.com>
2024-05-03 16:23:05 +0200

    set

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SOURCE_DIRECTORY=/tmp/app-logs
+mounika=/tmp/app-logs
 
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ -d $SOURCE_DIRECTORY ]
+if [ -d $mounika]
 then
     echo -e "$G mounika reddy directory exists $N"
 else
-    echo -e "$R Please make sure $SOURCE_DIRECTORY directory exists $N"
+    echo -e "$R Please make sure $mounika directory exists $N"
     exit 1
 fi
 
-FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+FILES=$(find $mounika -name "*.log" -mtime +14)
 
 echo "files to delete :$FILES"

2f8e7d1dcdc61a3c534cd365bd4b9dfa43d23253
Mounika <mounikaendukuri03@gmail.com>
2024-05-03 11:19:32 +0200

    learn

--- 11-current-script.sh
+++ 11-current-script.sh
@@ -5,6 +5,7 @@ COURSE="This is mounika reddy endukuri"
 echo "before calling other script : $COURSE"
 echo "process ID of current script:$$"
 
-./12-other-script.sh
+#./12-other-script.sh ( this method is will create 2 PID'S and the value variable value won't change)
+ source./12-other-script.sh
 
 echo "calling after the script: $COURSE"

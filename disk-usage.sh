3840ae46a93f72ce6cfede9f274b8978960b0536
Mounika <mounikaendukuri03@gmail.com>
2024-05-08 13:53:32 +0200

    deleting logs

--- disk-usage.sh
+++ disk-usage.sh
@@ -8,8 +8,8 @@ DISK_THRESHOLD=10
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print$6F}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print&NF}')
-    if [ $USAGE     -ge $DISK_USAGE ]
+    FOLDER=$(echo $line | awk -F " " '{print$NF}')
+    if [ $USAGE -ge $DISK_USAGE ]
     then   
         echo "$FOLDER is more than $DISK_THRESOLD"
     fi
